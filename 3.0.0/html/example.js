9d20852ec4d5f2ce7a79a10fcec39b3f27430920
EmidioStani <emidiostani@gmail.com>
2025-04-14 11:12:24 +0200

    change quote

--- 3.0.0/html/example.js
+++ 3.0.0/html/example.js
@@ -329,7 +329,7 @@ $(document).ready(function () {
 			console.log("select_list length:" + select_list.length)
 			$.each(select_list, function(i, select_item) {
 				version_list.forEach(function (item, index) {
-					option_item = "<option value=' + item + '>' + item + '</option>";
+					option_item = "<option value=" + item + ">" + item + "</option>";
 					select_item.append(option_item);
 				})
 			})
